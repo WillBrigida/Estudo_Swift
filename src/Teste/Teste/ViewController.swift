44e951a313c6c11f1da3617c8f6d919c98aa4951
Macbook <macbook@Macbook.local>
2023-07-17 15:57:00 -0300

    Primeiro commit

---
+++ src/Teste/Teste/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Teste
+//
+//  Created by Macbook on 17/07/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
